07e218fce71835d5ed601b735395278c1e4ef018
Shaun2398 <shaunsanmiguel@gmail.com>
2021-05-11 18:18:49 +0800

    add delay to row-fluid

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
-gsap.from('.row', {duration: 3, opacity: '0', ease: 'power4'})
-gsap.from('.row-fluid', {duration: 3, opacity: '0', ease: 'power4'})
+gsap.from('.row', {duration: 4, opacity: '0', ease: 'power4'})
+gsap.from('.row-fluid', {duration: 4, delay: 1, opacity: '0', ease: 'power4'})
